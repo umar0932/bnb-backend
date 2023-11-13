c0827b36c326f2b551fa3055be4080d00fc82a72
umar0932 <umar.khalid@chirptech.net>
2023-11-13 17:02:46 +0500

    validations applied

--- src/modules/users/dto/create-user.input.ts
+++ src/modules/users/dto/create-user.input.ts
@@ -1,40 +1,33 @@
 import { InputType, Field } from '@nestjs/graphql'
 import { Transform } from 'class-transformer'
 
-import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength, validate } from 'class-validator'
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm'
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
 
 @InputType()
 export class CreateUserInput {
   @Field(() => String)
   @IsString()
-  @IsEmail({}, { message: 'Email should be in proper format' })
-  @IsNotEmpty({
-    message: 'Email should not be empty'
-  })
+  @IsEmail()
+  @IsNotEmpty()
   email: string
 
   @Field(() => String)
-  @Transform(value => value.toString())
-  @IsNotEmpty({
-    message: 'First Name should not be empty'
-  })
-  @MinLength(3, { message: 'First Name minimum length is 3 characters' })
-  @MaxLength(15)
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   firstName: string
 
   @Field(() => String)
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
-  @MaxLength(15)
+  @MaxLength(50)
   lastName: string
 
   @Field(() => String)
-  // @Transform(value => value.toString())
-  @IsNotEmpty({
-    message: 'password should not be empty'
-  })
+  @IsString()
+  @IsNotEmpty()
   @MinLength(8)
   password: string
 
